435cb88b0f6800f6f70d10723b7e123f1e7c8102
dillank82 <dillankyuppi@gmail.com>
2025-12-19 15:02:23 +0500

    create InputForm component

---
+++ src/Components/InputForm/InputForm.tsx
@@ -0,0 +1,31 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+
+interface InputFormProps {
+    placeholderText: string
+    onSubmit: (inputValue: string) => void
+}
+
+export const InputForm = ({ placeholderText, onSubmit }: InputFormProps) => {
+    const [inputValue, setInputValue] = useState('')
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const input = event.currentTarget
+        const value = (input as HTMLInputElement).value
+        setInputValue(value)
+    }
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        onSubmit(inputValue)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                onChange={handleChange}
+                value={inputValue}
+                placeholder={placeholderText}
+            />
+        </form>
+    )
+}
