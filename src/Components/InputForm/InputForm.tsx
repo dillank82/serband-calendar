5698ca6f201181a486e3f052a7584a02be99118b
dillank82 <dillankyuppi@gmail.com>
2025-12-23 12:31:20 +0500

    refactor(ui): improve UX for  RealDayChanger and InputForm
    - RealDayChanger: improve opening UX by passing isOpen and handling state
    - InputForm: add label text, auto-focus specified in props; add element
      for error display; improve error style

--- src/Components/InputForm/InputForm.tsx
+++ src/Components/InputForm/InputForm.tsx
@@ -1,14 +1,18 @@
-import { ChangeEvent, FormEvent, ReactNode, useState } from "react"
+import { ChangeEvent, FormEvent, ReactNode, Ref, useState } from "react"
 import './InputForm.css'
 
 interface InputFormProps {
-    placeholderText: string
+    placeholderText?: string
     onSubmit: (inputValue: string) => void
     children?: ReactNode
     error?: string | null
+    id: string
+    labelText: string
+    autoFocus?: boolean
+    ref?: Ref<HTMLInputElement | null>
 }
 
-export const InputForm = ({ placeholderText, onSubmit, children, error }: InputFormProps) => {
+export const InputForm = ({ placeholderText, onSubmit, children, error, id, labelText, autoFocus, ref }: InputFormProps) => {
     const [inputValue, setInputValue] = useState('')
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -24,13 +28,18 @@ export const InputForm = ({ placeholderText, onSubmit, children, error }: InputF
 
     return (
         <form onSubmit={handleSubmit}>
+            <label htmlFor={id}>{labelText}</label>
             <input
+                id={id}
                 type="text"
                 onChange={handleChange}
                 value={inputValue}
                 placeholder={placeholderText}
-                className={error ? 'error' : ''}
+                autoFocus={autoFocus || false}
+                ref={ref}
+                aria-describedby={`${id}-alert`}
             />
+            <p role="alert" id={`${id}-alert`} className="input-error-message">{error}</p>
             <div className="form-sub-group">
                 {children}
                 <button type="submit" className="submit">Подтвердить</button>
