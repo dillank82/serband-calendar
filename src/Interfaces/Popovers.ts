fb57fc75fd47034090b18ca3360ddbea5301b478
dillank82 <dillankyuppi@gmail.com>
2025-12-25 15:03:26 +0500

    feat: add reusable Error and Message popover components based on Floating UI wrapper

---
+++ src/Interfaces/Popovers.ts
@@ -0,0 +1,24 @@
+import { FloatingContext } from "@floating-ui/react";
+import { CSSProperties, ReactNode, Ref } from "react";
+
+export interface PopoverWrapperProps {
+    children: ReactNode
+    isOpen: boolean
+    floatingRef: Ref<HTMLDivElement>
+    arrowRef: Ref<SVGSVGElement>
+    floatingStyles: CSSProperties
+    context: FloatingContext
+    arrowStyles?: CSSProperties
+}
+
+export type PopoverMessageProps = {
+    content: ReactNode
+    title?: string
+    className?: string
+    ariaRole: 'alert' | 'status' | 'tooltip'
+} & Omit<PopoverWrapperProps, 'children'>
+
+export type PopoverErrorProps = (
+    Omit<PopoverMessageProps, 'content' | 'ariaRole' | 'className' | 'children'> 
+    & { message: string }
+) 
